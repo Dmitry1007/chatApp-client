c31a1fa2025bee4643af457a750cddd825652ccd
Dmitry Sky <dmitry.sky@icloud.com>
2023-05-03 12:36:10 -0600

    react dropzone to add attachments

--- src/components/customMessageForms/StandardMessageForm.jsx
+++ src/components/customMessageForms/StandardMessageForm.jsx
@@ -1,10 +1,15 @@
-import { XMarkIcon } from "@heroicons/react/24/solid";
+import { PaperClipIcon, XMarkIcon } from "@heroicons/react/24/solid";
 import { useState } from "react";
+import Dropzone from "react-dropzone";
 
 function StandardMessageForm() {
     const [message, setMessage] = useState("");
     const [attachement, setAttachement] = useState("");
     const [previewAttachement, setPreviewAttachement] = useState("");
+
+    const handleChange = (event) => {
+        setMessage(event.target.value);
+    };
 
     return (
         <div className="message-form-container">
@@ -25,7 +30,40 @@ function StandardMessageForm() {
                     />
                 </div>
             )}
-            <div>Message Form</div>
+            <div className="message-form">
+                <div className="message-form-input-container">
+                    <input
+                        className="message-form-input"
+                        type="text"
+                        value={message}
+                        onChange={handleChange}
+                        placeholder="Type a message..."
+                    />
+                </div>
+                <div className="message-form-icons">
+                    <Dropzone
+                        acceptedFiles=".jpg, .jpeg, .png"
+                        multiple={false}
+                        noClick={true}
+                        onDrop={(acceptedFiles) => {
+                            setAttachement(acceptedFiles[0]);
+                            setPreviewAttachement(
+                                URL.createObjectURL(acceptedFiles[0])
+                            );
+                        }}
+                    >
+                        {({ getRootProps, getInputProps, open }) => (
+                            <div {...getRootProps()}>
+                                <input {...getInputProps()} />
+                                <PaperClipIcon
+                                    className="message-form-icon-clip"
+                                    onClick={open}
+                                />
+                            </div>
+                        )}
+                    </Dropzone>
+                </div>
+            </div>
         </div>
     );
 }
