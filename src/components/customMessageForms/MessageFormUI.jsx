e41d915eec1204cb1ce7ba55342f4e291cc0db34
Dmitry Sky <dmitry.sky@icloud.com>
2023-05-03 15:38:38 -0600

    Refactor into more reusable component

---
+++ src/components/customMessageForms/MessageFormUI.jsx
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import {
+    PaperAirplaneIcon,
+    PaperClipIcon,
+    XMarkIcon,
+} from "@heroicons/react/24/solid";
+import Dropzone from "react-dropzone";
+
+function MessageFormUI({ setAttachment, message, handleChange, handleSubmit }) {
+    const [previewAttachment, setPreviewAttachment] = useState("");
+
+    return (
+        <div className="message-form-container">
+            {previewAttachment && (
+                <div className="message-form-preview">
+                    <img
+                        className="message-form-preview-image"
+                        src={previewAttachment}
+                        onLoad={() => URL.revokeObjectURL(previewAttachment)}
+                        alt="message-form-preview-attachement"
+                    />
+                    <XMarkIcon
+                        className="message-form-icon-x"
+                        onClick={() => {
+                            setPreviewAttachment("");
+                            setAttachment("");
+                        }}
+                    />
+                </div>
+            )}
+            <div className="message-form">
+                <div className="message-form-input-container">
+                    <input
+                        className="message-form-input"
+                        type="text"
+                        value={message}
+                        onChange={handleChange}
+                        placeholder="Type a message..."
+                    />
+                </div>
+                <div className="message-form-icons">
+                    <Dropzone
+                        acceptedFiles=".jpg, .jpeg, .png"
+                        multiple={false}
+                        noClick={true}
+                        onDrop={(acceptedFiles) => {
+                            setAttachment(acceptedFiles[0]);
+                            setPreviewAttachment(
+                                URL.createObjectURL(acceptedFiles[0])
+                            );
+                        }}
+                    >
+                        {({ getRootProps, getInputProps, open }) => (
+                            <div {...getRootProps()}>
+                                <input {...getInputProps()} />
+                                <PaperClipIcon
+                                    className="message-form-icon-clip"
+                                    onClick={open}
+                                />
+                            </div>
+                        )}
+                    </Dropzone>
+                    <hr className="vertical-line" />
+                    <PaperAirplaneIcon
+                        className="message-form-icon-airplane"
+                        onClick={() => {
+                            setPreviewAttachment("");
+                            handleSubmit();
+                        }}
+                    />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default MessageFormUI;
--- src/components/customMessageForms/StandardMessageForm.jsx
+++ src/components/customMessageForms/StandardMessageForm.jsx
@@ -1,15 +1,9 @@
-import {
-    PaperAirplaneIcon,
-    PaperClipIcon,
-    XMarkIcon,
-} from "@heroicons/react/24/solid";
 import { useState } from "react";
-import Dropzone from "react-dropzone";
+import MessageFormUI from "./MessageFormUI";
 
 function StandardMessageForm({ props, activeChat }) {
     const [message, setMessage] = useState("");
     const [attachment, setAttachment] = useState("");
-    const [previewAttachment, setPreviewAttachment] = useState("");
 
     const handleChange = (event) => {
         setMessage(event.target.value);
@@ -37,70 +31,13 @@ function StandardMessageForm({ props, activeChat }) {
     };
 
     return (
-        <div className="message-form-container">
-            {previewAttachment && (
-                <div className="message-form-preview">
-                    <img
-                        className="message-form-preview-image"
-                        src={previewAttachment}
-                        onLoad={() => URL.revokeObjectURL(previewAttachment)}
-                        alt="message-form-preview-attachement"
-                    />
-                    <XMarkIcon
-                        className="message-form-icon-x"
-                        onClick={() => {
-                            setPreviewAttachment("");
-                            setAttachment("");
-                        }}
-                    />
-                </div>
-            )}
-            <div className="message-form">
-                <div className="message-form-input-container">
-                    <input
-                        className="message-form-input"
-                        type="text"
-                        value={message}
-                        onChange={handleChange}
-                        placeholder="Type a message..."
-                    />
-                </div>
-                <div className="message-form-icons">
-                    <Dropzone
-                        acceptedFiles=".jpg, .jpeg, .png"
-                        multiple={false}
-                        noClick={true}
-                        onDrop={(acceptedFiles) => {
-                            setAttachment(acceptedFiles[0]);
-                            setPreviewAttachment(
-                                URL.createObjectURL(acceptedFiles[0])
-                            );
-                        }}
-                    >
-                        {({ getRootProps, getInputProps, open }) => (
-                            <div {...getRootProps()}>
-                                <input {...getInputProps()} />
-                                <PaperClipIcon
-                                    className="message-form-icon-clip"
-                                    onClick={open}
-                                />
-                            </div>
-                        )}
-                    </Dropzone>
-                    <hr className="vertical-line" />
-                    <PaperAirplaneIcon
-                        className="message-form-icon-airplane"
-                        onClick={() => {
-                            setPreviewAttachment("");
-                            handleSubmit();
-                        }}
-                    />
-                </div>
-            </div>
-        </div>
+        <MessageFormUI
+            setAttachment={setAttachment}
+            message={message}
+            handleChange={handleChange}
+            handleSubmit={handleSubmit}
+        />
     );
 }
 
 export default StandardMessageForm;
-
-// This is where we could record audio and send it as a message
