8a0661d339dcef6dc4cd88f4e369dd0abe9b8d64
Dmitry Sky <dmitry.sky@icloud.com>
2023-05-02 19:01:04 -0600

    Setup header component that for some reason doesn't display on the webpage...

--- src/components/chat/index.jsx
+++ src/components/chat/index.jsx
@@ -1,5 +1,29 @@
+import {
+    useMultiChatLogic,
+    MultiChatSocket,
+    MultiChatWindow,
+} from "react-chat-engine-advanced";
+import Header from "@/components/customHeader";
+
 function Chat() {
-    return <div>Chat</div>;
+    const chatProps = useMultiChatLogic(
+        import.meta.env.VITE_PROJECT_ID,
+        "testuser1",
+        "12345"
+    );
+
+    return (
+        <div style={{ flexBasis: "100%" }}>
+            <MultiChatSocket {...chatProps} />
+            <MultiChatWindow
+                {...chatProps}
+                style={{ height: "100vh" }}
+                renderChatHeader={(chat) => {
+                    <Header chat={chat} />;
+                }}
+            />
+        </div>
+    );
 }
 
 export default Chat;
